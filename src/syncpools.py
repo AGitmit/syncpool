e4878404c70b54b091c064e934581cc2db5927a3
Amit Nakash <87775966+AGitmit@users.noreply.github.com>
2024-04-24 22:53:00 +0300

    add pid validation

--- src/syncpools.py
+++ src/syncpools.py
@@ -1,9 +1,11 @@
 import asyncio
 import threading
 
+from os import getpid
 from collections import deque
-from typing import Any
+from typing import Any, Callable
 from generic_obj import GenericObject
+from exceptions import IllegalProcessAccessError
 
 
 class ObjectPool:
@@ -13,14 +15,30 @@ class ObjectPool:
     with the main goal of minimizing memory related overhead by reusing already allocated memory.
 
     Attributes:
-        - size (int | None) : set a maximum objects capacity on the stack.
-        - obj (Any) : an object class of any type - will be used as a default object to instanciate when stack is empty.
+        cap (int | None): The maximum capacity of objects in the pool. If None, the pool has unlimited capacity.
+        obj (Any): The class of objects to be managed by the pool. Defaults to GenericObject.
+        on_put (Callable | None): Optional callback function to be executed when an object is put into the pool.
+        on_get (Callable | None): Optional callback function to be executed when an object is retrieved from the pool.
     """
 
-    def __init__(self, cap: int | None = None, obj: Any = None) -> None:
+    def __init__(
+        self,
+        cap: int | None = None,
+        obj: Any = None,
+        on_put: Callable | None = None,
+        on_get: Callable | None = None,
+    ) -> None:
         self.stack = deque(maxlen=cap)
         self.obj = obj or GenericObject
+        self.on_put = on_put
+        self.on_get = on_get
+
         self._lock = threading.Lock()
+        self._pool_pid = getpid()
+
+    def _validate_process(self) -> None:
+        if self._pool_pid != getpid():
+            raise IllegalProcessAccessError()
 
     def count(self) -> int:
         "Returns the number of elements in the stack"
@@ -34,13 +52,21 @@ class ObjectPool:
 
     def get(self) -> Any:
         "Get an element from the top of the stack (LIFO)"
+        self._validate_process()
         with self._lock:
-            if self.count() > 0:
-                return self.stack.pop()
-            return self.obj()
+            if len(self.stack) > 0:
+                x = self.stack.pop()
+            else:
+                x = self.obj()
+        if self.on_get:
+            self.on_get(x)
+        return x
 
     def put(self, x: Any) -> None:
         "Put an element to the top of the stack"
+        self._validate_process()
+        if self.on_put:
+            self.on_put(x)
         with self._lock:
             self.stack.append(x)
 
@@ -52,14 +78,30 @@ class AsyncObjectPool:
     with the main goal of minimizing memory related overhead by reusing already allocated memory.
 
     Attributes:
-        - size (int | None) : set a maximum objects capacity on the stack.
-        - obj (Any) : an object class of any type - will be used as a default object to instanciate when stack is empty.
+        cap (int | None): The maximum capacity of objects in the pool. If None, the pool has unlimited capacity.
+        obj (Any): The class of objects to be managed by the pool. Defaults to GenericObject.
+        on_put (Callable | None): Optional callback function to be executed when an object is put into the pool.
+        on_get (Callable | None): Optional callback function to be executed when an object is retrieved from the pool.
     """
 
-    def __init__(self, cap: int | None = None, obj: Any = None) -> None:
+    def __init__(
+        self,
+        cap: int | None = None,
+        obj: Any = None,
+        on_put: Callable | None = None,
+        on_get: Callable | None = None,
+    ) -> None:
         self.stack = deque(maxlen=cap)
         self.obj = obj or GenericObject
+        self.on_put = on_put
+        self.on_get = on_get
+
         self._lock = asyncio.Lock()
+        self._pool_pid = getpid()
+
+    def _validate_process(self) -> None:
+        if self._pool_pid != getpid():
+            raise IllegalProcessAccessError()
 
     async def async_count(self) -> int:
         "Returns the number of elements in the stack"
@@ -72,12 +114,20 @@ class AsyncObjectPool:
 
     async def async_get(self) -> Any:
         "Get() coroutine-safe; Mot thread-safe."
-        if await self.async_count() > 0:
-            async with self._lock:
-                return self.stack.pop()
-        return self.obj()
+        self._validate_process()
+        async with self._lock:
+            if await len(self.stack) > 0:
+                x = self.stack.pop()
+            else:
+                x = self.obj()
+        if self.on_get:
+            self.on_get(x)
+        return x
 
     async def async_put(self, x: Any) -> None:
         "Put() coroutine-safe; Mot thread-safe."
+        self._validate_process()
+        if self.on_put:
+            self.on_put(x)
         async with self._lock:
             self.stack.append(x)
