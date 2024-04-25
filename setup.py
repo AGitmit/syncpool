22b5df2906a1970544ee8201c31a8eb7a1d2abe3
Amit Nakash <87775966+AGitmit@users.noreply.github.com>
2024-04-26 00:34:47 +0300

    update setup.py make manifest.in

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="syncpool-py",
     description="Synchronization object-pools, imitating Go's (Golang) sync.Pool implementation.",
-    packages=find_packages(exclude=('tests',)),
+    package_dir={'': 'src'},
+    packages=find_packages(where='src', exclude=['src.tests']),
     author="Amit Nakash",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
