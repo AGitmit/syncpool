f0092f9a3bdca3642b178a90faa8414285a96b4c
Amit Nakash <87775966+AGitmit@users.noreply.github.com>
2024-04-26 00:16:43 +0300

    add pyproject.toml

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="syncpool",
+    name="syncpool-py",
     description="Synchronization object-pools, imitating Go's (Golang) sync.Pool implementation.",
-    packages=find_packages(),
+    packages=find_packages(exclude=('tests',)),
     author="Amit Nakash",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
